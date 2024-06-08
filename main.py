12746d658c51ce5bf4c314dbe758c39d9b543b91
athifafajri <athifa.fahmawati@gmail.com>
2024-06-08 14:09:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,15 @@ def run():
 
          div.st-emotion-cache-1r6slb0 span.st-emotion-cache-10trblm{
             font: bold 24px tahoma;
-            
+         }
+         div [data-testid=stImage]{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+        }
+
         div[data-baseweb=select]>div{
             cursor: pointer;
             background-color: #111;
@@ -164,6 +172,7 @@ def run():
 
     with st.sidebar:
         st.title("SPENT MONEY :blue[PREDICTION]")
+        st.image("imgs/money.png", caption="", width=60)
         page = option_menu(
             menu_title=None,
             options=['Home', 'Relations & Correlarions', 'Prediction'],
@@ -286,12 +295,17 @@ def run():
                                 predicted_col, score_col = st.columns(2)
 
                                 with predicted_col:
+                                    st.image("imgs/money-bag.png",
+                                             caption="", width=70)
+
                                     st.subheader(
                                         "Expected To Spent")
                                     st.subheader(
                                         f"${np.round(predicted_value[0], 2)}")
 
                                 with score_col:
+                                    st.image("imgs/star.png",
+                                             caption="", width=70)
                                     st.subheader("Model Accuracy")
                                     st.subheader(f"{np.round(98.27, 2)}%")
 
@@ -338,7 +352,27 @@ def run():
                             if actual_file is not None and test_file is not None:
                                 y_test = pd.read_csv(actual_file)
                                 if y_test.shape[1] == 1:
- 
+
+                                    col1, col2 = st.columns(2)
+
+                                    with col1:
+                                        test_score = np.round(
+                                            model.score(X_test, y_test) * 100, 2)
+                                        prediction.creat_matrix_score_cards("imgs/star.png",
+                                                                            "Prediction Accuracy",
+                                                                            test_score,
+                                                                            True
+                                                                            )
+
+                                    with col2:
+                                        mae = mean_absolute_error(
+                                            y_test, all_predicted_values)
+                                        prediction.creat_matrix_score_cards("imgs/sort.png",
+                                                                            "Error Ratio",
+                                                                            np.round(
+                                                                                mae, 2),
+                                                                            False)
+
                                     predicted_df = prediction.create_comparison_df(
                                         y_test, all_predicted_values)
                                     st.dataframe(
