1b4192ddeb195aa8a5dd581ce27c760f6d0b4eab
athifafajri <athifa.fahmawati@gmail.com>
2024-06-08 14:07:18 +0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -10,22 +10,6 @@ pd.set_option('future.no_silent_downcasting', True)
 pd.options.mode.copy_on_write = "warn"
 
 
-def creat_matrix_score_cards(card_image="", card_title="Card Title", card_value=None, percent=False):
-    st.image(card_image,
-             caption="", width=70)
-
-    st.subheader(
-        card_title)
-
-    if percent:
-        st.subheader(
-            f"{card_value}%")
-
-    else:
-        st.subheader(
-            f"{card_value}")
-
-
 def create_comparison_df(y_actual, y_pred):
     predected_df = pd.DataFrame()
     predected_df["Actual Spent Values"] = y_actual
